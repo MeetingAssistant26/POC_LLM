a5c348a7efabe435f3017b747a74c0b43131b90f
Lojain Mohamed <lojainmohamed795@gmail.com>
2026-03-17 03:15:44 +0200

    Add audio preprocessing for whisperx

--- audio_pipeline/audio_preprocessing.py
+++ audio_pipeline/audio_preprocessing.py
@@ -1,5 +1,6 @@
 import os
 from pydub import AudioSegment
+from pydub.silence import split_on_silence
 
 input_folder = "audio"
 output_folder = "audio_clean"
@@ -17,9 +18,29 @@ for file in os.listdir(input_folder):
 
         audio = AudioSegment.from_file(input_path)
 
+        # Convert to mono
         audio = audio.set_channels(1)
+
+        # Convert sample rate
         audio = audio.set_frame_rate(16000)
 
-        audio.export(output_path, format="wav")
+        # Normalize volume
+        change_in_dBFS = -20.0 - audio.dBFS
+        audio = audio.apply_gain(change_in_dBFS)
+
+        # Remove long silence
+        chunks = split_on_silence(
+            audio,
+            min_silence_len=800,
+            silence_thresh=audio.dBFS - 14,
+            keep_silence=300
+        )
+
+        processed_audio = AudioSegment.empty()
+
+        for chunk in chunks:
+            processed_audio += chunk
+
+        processed_audio.export(output_path, format="wav")
 
 print("Audio preprocessing completed.")
